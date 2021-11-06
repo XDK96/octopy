aacb5733fd16742d10a8c2ad5e767f285d9b1ad7
XDK96 <43788087+XDK96@users.noreply.github.com>
2021-11-06 20:36:44 +0100

    fix

--- octopy/elo/utils.py
+++ octopy/elo/utils.py
@@ -8,15 +8,11 @@ __EPS__ = 1e-12
 def predict_proba(params, teamA_rating, teamB_rating, has_tie):
     dr = (teamA_rating - teamB_rating) * params["beta"]
     gamma = nn.relu(params["gamma"]) * has_tie
-    print(gamma)
-    print(has_tie)
-    print(teamA_rating,":",teamB_rating)
     pA = jnp.clip(nn.sigmoid(dr - gamma), __EPS__, 1 - __EPS__)
     pB = jnp.clip(nn.sigmoid(-dr - gamma), __EPS__, 1 - __EPS__)
     pD = nn.relu(1.0 - pA - pB) * has_tie
     s = pA + pB + pD
-    # return [jnp.array(x, float) for x in [pA / s, pD / s, pB / s]]
-    return [jnp.array(x, float) for x in [gamma, has_tie, teamA-rating, teamB_rating]]
+    return [jnp.array(x, float) for x in [pA / s, pD / s, pB / s]]
 @jit
 def get_log_loss(scoreA, scoreB, pA, pB, pD=0):
     '''return the log loss given the score and probabilities.'''
